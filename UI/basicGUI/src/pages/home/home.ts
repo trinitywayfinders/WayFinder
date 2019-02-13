e2740a1b61f14f1c8cafd9410703365baf08e7c3
Mark Grennan <grennama@tcd.ie>
2019-02-13 12:06:22 +0000

    WD-47: first commit. Contains map using leaflet and openStreetMap. Two text boxes and button.

---
+++ UI/basicGUI/src/pages/home/home.ts
@@ -0,0 +1,29 @@
+
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import leaflet from 'leaflet';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidEnter() {
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 18
+    }).addTo(this.map);
+  }
+
+}
