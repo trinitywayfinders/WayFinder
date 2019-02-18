9cba04641bed602b410de85515167aa526cd2a02
Mark Grennan <grennama@tcd.ie>
2019-02-18 13:10:21 +0000

    WD:47 basic GUI

--- UI/basicGUI/src/pages/home/home.ts
+++ UI/basicGUI/src/pages/home/home.ts
@@ -3,7 +3,6 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import leaflet from 'leaflet';
-import axios from 'axios';
 
 @Component({
   selector: 'page-home',
@@ -29,24 +28,4 @@ export class HomePage {
       maxZoom: 18
     }).addTo(this.map);
   }
-
-  // post to db and create alert box if successful, else log error
-  postToDB() {
-    axios.post('http://localhost:3000/routing', {
-      location: this.inputLocation,
-      destination: this.inputDestination,
-    }).then(resp => {
-      console.log(resp.data);
-      let alert = this.alertCtrl.create({
-        title: "Routing...",
-        subTitle: this.inputLocation + " to " + this.inputDestination,
-        buttons: ['OK']
-      });
-      alert.present();
-    }).catch(error => {
-      console.log(error);
-    });
-  }
-
-
 }
