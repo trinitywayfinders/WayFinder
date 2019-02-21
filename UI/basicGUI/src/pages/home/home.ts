fc35725fa2fa04d2419e1af852f7df8ff6926b04
Nicky <bonellon@tcd.ie>
2019-02-21 15:41:09 +0000

    WD-67: Added ShowLocation button that returns coordinates

--- UI/basicGUI/src/pages/home/home.ts
+++ UI/basicGUI/src/pages/home/home.ts
@@ -11,74 +11,55 @@ import leaflet from 'leaflet';
 export class HomePage {
   @ViewChild('map') mapContainer: ElementRef;
   map: any;
-  currentLatlng: any;
   inputLocation = '';
   inputDestination = '';
+  currentLatlng: any;
+  marker: leaflet.market
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
 
+  ionViewDidEnter() {
+    this.loadmap();
+  }
 
-   getLocation():any{
-      this.map.locate({
-      }).on('locationfound', (e) => {
+   getLocation(){
+     this.map.locate({
+     }).once('locationfound', (e) => {
+      console.log("ONCE")
+      this.currentLatlng = e.latlng
 
-        //console.log('in locationFound '+currentLatlng.latlng)
+      let markerGroup = leaflet.featureGroup();
+      if (this.marker) { // check
+        this.map.removeLayer(this.marker); // remove
+      }
+      this.marker = leaflet.marker([e.latitude, e.longitude])
+      markerGroup.addLayer(this.marker);
+      this.map.addLayer(markerGroup);
 
-        this.currentLatlng = e
-
-        this.getCurrentLatLing()
       }).on('locationerror', (err) => {
           console.log(err.message);
       })
   }
 
-  getCurrentLatLing(){
-    let alertOfLoc = this.alertCtrl.create({
-      title:"YOUR CURRENT LOCATION",
-      subTitle: "Latitude:" + this.currentLatlng.latitude+" Longitude: " +this.currentLatlng.longitude,
-      buttons:['GOT IT']
-    });
+    showLocation(){
+      this.getLocation()
+
+      let alertOfLoc = this.alertCtrl.create({
+        title:"YOUR CURRENT LOCATION",
+        subTitle: "Latitude:" + this.currentLatlng.latitude+" Longitude: " +this.currentLatlng.longitude,
+        buttons:['GOT IT']
+      });
     alertOfLoc.present();
   }
 
-    ionViewDidEnter() {
-      this.loadmap();
-    }
   loadmap() {
+    console.log("Loading map...")
     this.map = leaflet.map("map").fitWorld();
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attributions: 'www.tphangout.com',
       maxZoom: 18
     }).addTo(this.map);
-
-    ///////Mark the current location
-    this.map.locate({
-      //setView: true,
-      maxZoom: 10
-    }).on('locationfound', (e) => {
-      //this.currentLatlng = e;
-      //alert(e.latlng);
-      let markerGroup = leaflet.featureGroup();
-      let marker: any = leaflet.marker([e.latitude, e.longitude])
-      markerGroup.addLayer(marker);
-      this.map.addLayer(markerGroup);
-      }).on('locationerror', (err) => {
-        alert(err.message);
-    })
-
-
-    //console.log(currentLatlng)
-  //  console.log("WHAT")
-
-
-    let markerGroup = leaflet.featureGroup();
-
-
-    //let marker: any = leaflet.marker([currentLatlng[0], currentLatlng[1]]).on('click', () => {
-    //    alert('Current Location!');
-      //})
-    //markerGroup.addLayer(marker);
-    //this.map.addLayer(markerGroup);
+    this.getLocation()
   }
 }
