8fc150796c1876ef5bc7a722b5ec53357ba01a06
Saad Tariq Malik <maliksa@tcd.ie>
2019-02-26 12:45:06 +0000

    SpringBoot Service Template For Eclipse

---
+++ Code/SpringBootProjectTemplate/src/main/java/com/devglan/SpringBootProjectTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.devglan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectTemplateApplication.class, args);
+	}
+
+}
