c726ac518d2c24da616e9b7cb22e8b1184a1ea45
Nicky <bonellon@tcd.ie>
2019-02-27 10:36:16 +0000

    WD-65: Removed static methods, responseXML

--- Code/ETCDLibrary/src/main/java/ie/tcd/wayfinder/etcd/ETCDLibrary.java
+++ Code/ETCDLibrary/src/main/java/ie/tcd/wayfinder/etcd/ETCDLibrary.java
@@ -5,87 +5,58 @@ import ie.tcd.wayfinders.restLibrary.Library;
 import java.io.IOException;
 import java.util.Optional;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
 
 public class ETCDLibrary {
 
-	private static String baseUrl = "http://35.246.14.12:2379/v2/keys/";
+	private String baseUrl = "http://35.246.14.12:2379/v2/keys/";
 		
-	public static String ReadDirectory(String directoryName) throws IOException {
+	public HttpResponse ReadDirectory(String directoryName) throws IOException {
 		
 		String url = (baseUrl + directoryName).trim();
 		HttpResponse response = Library.GET(url, Optional.empty());
-		
-		 HttpEntity responseEntity = response.getEntity();
-	     String responseXml = EntityUtils.toString(responseEntity);
-	     
-	     return responseXml;
+		return response;
 		
 	}
 	
-	public static String DeleteDirectory(String directoryName) throws IOException {
+	public HttpResponse DeleteDirectory(String directoryName) throws IOException {
 		
 		String url = baseUrl + directoryName+"?dir=true";
-		
 		HttpResponse response = Library.DELETE(url, Optional.empty());
-		
-		HttpEntity responseEntity = response.getEntity();
-	    String responseXml = EntityUtils.toString(responseEntity);
-	     
-	    return responseXml;
+		return response;
 	
 	}
 	
-	public static String CreateDirectory(String directoryName) throws IOException {
+	public HttpResponse CreateDirectory(String directoryName) throws IOException {
 		
 		String url = baseUrl + directoryName + "?dir=true";
 		HttpResponse response = Library.PUT(url, Optional.empty(), Optional.empty());
-		
-		 HttpEntity responseEntity = response.getEntity();
-	     String responseXml = EntityUtils.toString(responseEntity);
-	     
-	     return responseXml;
+		return response;
 	}
 
-	public static String CreateKey(String directoryName, String key, String value) throws IOException {
+	public HttpResponse CreateKey(String directoryName, String key, String value) throws IOException {
 		
 		String url = baseUrl + directoryName + "/"+key+"?value="+value;
 		HttpResponse response = Library.PUT(url, Optional.empty(), Optional.empty());
-		
-		HttpEntity responseEntity = response.getEntity();
-	    String responseXml = EntityUtils.toString(responseEntity);
-	     
-	    return responseXml;
+		return response;
 	}
 
-	public static String UpdateKey(String directoryName, String key, String value) throws IOException {
+	public HttpResponse UpdateKey(String directoryName, String key, String value) throws IOException {
 
 		return CreateKey(directoryName, key, value);
 	}
 
-
-	public static String ReadKey(String directoryName, String key) throws IOException {
-		String url = baseUrl + directoryName;
-
-		HttpResponse response = Library.GET(url, Optional.empty());
+	public HttpResponse ReadKey(String directoryName, String key) throws IOException {
 		
-		HttpEntity responseEntity = response.getEntity();
-	    String responseXml = EntityUtils.toString(responseEntity);
-	     
-	    return responseXml;
+		String url = baseUrl + directoryName;
+		HttpResponse response = Library.GET(url, Optional.empty()); 
+	    return response;
 	}
 
-	public static String DeleteKey(String directoryName, String key) throws IOException {
+	public HttpResponse DeleteKey(String directoryName, String key) throws IOException {
 
 		String url = baseUrl + directoryName + "/"+key;
-
 		HttpResponse response = Library.DELETE(url, Optional.empty());
-		
-		HttpEntity responseEntity = response.getEntity();
-	    String responseXml = EntityUtils.toString(responseEntity);
-	     
-	    return responseXml;
+		return response;
 	}
 }
--- Code/ETCDLibrary/src/test/java/ie/tcd/wayfinder/etcd/test/ETCDLibraryTest.java
+++ Code/ETCDLibrary/src/test/java/ie/tcd/wayfinder/etcd/test/ETCDLibraryTest.java
@@ -2,6 +2,10 @@ package ie.tcd.wayfinder.etcd.test;
 
 import static org.junit.Assert.assertTrue;
 import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,140 +15,153 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ie.tcd.wayfinder.etcd.ETCDLibrary;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ETCDLibrary.class)
 @AutoConfigureMockMvc
 public class ETCDLibraryTest {
-		
+
 	@Autowired
-    private MockMvc mvc;
-	
+	private MockMvc mvc;
+
 	String directoryName = "test";
 	String key = "testKey";
 	String value = "testValue";
-    
-    @Test
-    public void createDirectory() throws IOException {
-    	
-    	String expectedOutput = "{\"action\":\"set\",\"node\":{\"key\":\"/test\",\"dir\":true";
-    	    	    	    	
-    	if(directoryExists(directoryName)) 
-    		deleteDirectory(directoryName);
-    	
-    	String response = ETCDLibrary.CreateDirectory(directoryName);        
-    	assertTrue(response.contains(expectedOutput));
-    }
-        
-    @Test
-    public void readDirectory() throws IOException {
+	ETCDLibrary library = new ETCDLibrary();
 
-    	String expectedOutput = "{\"action\":\"get\",\"node\":{\"key\":\"/test\",\"dir\":true";
-    	
-    	if(!directoryExists(directoryName))
-    		createDirectory(directoryName);
-    	
-    	String response = ETCDLibrary.ReadDirectory(directoryName);
-    	assertTrue(response.contains(expectedOutput));
-    }
-    
-    @Test
-    public void removeDirectory() throws IOException {
-    	
-    	String expectedOutput = "{\"action\":\"delete\",\"node\":{\"key\":\"/test\",\"dir\":true,\"";
-    	
-    	if(!directoryExists(directoryName))
-    		createDirectory(directoryName);
-    	
-    	String response = ETCDLibrary.DeleteDirectory(directoryName);
-    	assertTrue(response.contains(expectedOutput));
-    }
-    
-    @Test
-    public void createKey() throws IOException {
-    	String expectedOutput = "{\"action\":\"set\",\"node\":{\"key\":\"/test/testKey\",\"value\":\"testValue\"";
-    	
+	@Test
+	public void createDirectory() throws IOException {
 
-    	if(keyExists(directoryName, key))
-    		deleteKey(directoryName, key);
-    	
-    	String response = ETCDLibrary.CreateKey(directoryName, key, value);
-    	assertTrue(response.contains(expectedOutput));
-    	deleteKey(directoryName, key);
-    }
-    
-    @Test
-    public void updateKey() throws IOException {
-    	String expectedOutput = "{\"key\":\"/test/testKey\",\"value\":\"newValue2\"";
-    	
-    	String update = "newValue2";
-    	
-    	if(!keyExists(directoryName, key))
-    		createKey(directoryName, key, value);
-    	
-    	String response = ETCDLibrary.UpdateKey(directoryName, key, update);
-    	assertTrue(response.contains(expectedOutput));
-    	deleteKey(directoryName, key);
-    }
-    
-    @Test
-    public void deleteKey() throws IOException {
-    	String expectedOutput = "{\"key\":\"/test/testKey\",\"value\":\"testValue\"";
-    	
+		String expectedOutput = "{\"action\":\"set\",\"node\":{\"key\":\"/test\",\"dir\":true";
 
-    	if(!keyExists(directoryName, key))
-    		createKey(directoryName, key, value);
-    	
-    	String response = ETCDLibrary.CreateKey(directoryName, key, value);
-    	assertTrue(response.contains(expectedOutput));
-    }
-    
-    @Test
-    public void readKey() throws IOException {
-    	String expectedOutput = "{\"key\":\"/test/testKey\",\"value\":\"testValue\"";
-    	
-    	if(!keyExists(directoryName, key))
-    		createKey(directoryName, key, value);
-    	
-    	String response = ETCDLibrary.CreateKey("test", key, value);
-    	assertTrue(response.contains(expectedOutput));
+		if (directoryExists(directoryName))
+			deleteDirectory(directoryName);
 
-    	deleteKey(directoryName, key);
+		HttpResponse response = library.CreateDirectory(directoryName);
+		String responseString = EntityUtils.toString(response.getEntity());
+
+		assertTrue(responseString.contains(expectedOutput));
+	}
+
+	@Test
+	public void readDirectory() throws IOException {
+
+		String expectedOutput = "{\"action\":\"get\",\"node\":{\"key\":\"/test\",\"dir\":true";
+
+		if (!directoryExists(directoryName))
+			createDirectory(directoryName);
+
+		HttpResponse response = library.ReadDirectory(directoryName);
+		String responseString = EntityUtils.toString(response.getEntity());
+
+		assertTrue(responseString.contains(expectedOutput));
+	}
+
+	@Test
+	public void removeDirectory() throws IOException {
+
+		String expectedOutput = "{\"action\":\"delete\",\"node\":{\"key\":\"/test\",\"dir\":true,\"";
+
+		if (!directoryExists(directoryName))
+			createDirectory(directoryName);
+
+		HttpResponse response = library.DeleteDirectory(directoryName);
+		String responseString = EntityUtils.toString(response.getEntity());
+
+		assertTrue(responseString.contains(expectedOutput));
+	}
+
+	@Test
+	public void createKey() throws IOException {
+		String expectedOutput = "{\"action\":\"set\",\"node\":{\"key\":\"/test/testKey\",\"value\":\"testValue\"";
+
+		if (keyExists(directoryName, key))
+			deleteKey(directoryName, key);
+
+		HttpResponse response = library.CreateKey(directoryName, key, value);
+		String responseString = EntityUtils.toString(response.getEntity());
+
+		assertTrue(responseString.contains(expectedOutput));
+		deleteKey(directoryName, key);
+	}
+
+	@Test
+	public void updateKey() throws IOException {
+		String expectedOutput = "{\"key\":\"/test/testKey\",\"value\":\"newValue2\"";
+
+		String update = "newValue2";
+
+		if (!keyExists(directoryName, key))
+			createKey(directoryName, key, value);
+
+		HttpResponse response = library.UpdateKey(directoryName, key, update);
+		String responseString = EntityUtils.toString(response.getEntity());
+
+		assertTrue(responseString.contains(expectedOutput));
+		deleteKey(directoryName, key);
+	}
+
+	@Test
+	public void deleteKey() throws IOException {
+		String expectedOutput = "{\"key\":\"/test/testKey\",\"value\":\"testValue\"";
+
+		if (!keyExists(directoryName, key))
+			createKey(directoryName, key, value);
+
+		HttpResponse response = library.CreateKey(directoryName, key, value);
+		String responseString = EntityUtils.toString(response.getEntity());
     	
-    }
-    
-    private boolean directoryExists(String directoryName) throws IOException {
+    	assertTrue(responseString.contains(expectedOutput));
+    	}
 
-    	String response = ETCDLibrary.ReadDirectory(directoryName);
-    	if(response.contains("{\"action\":\"get\",\"node\":{\"key\":\"/"+directoryName+"\",\"dir\":true"))
-    		return true;
-    	return false;
-    }
-    
-    private boolean keyExists(String directoryName, String key) throws IOException {
+	@Test
+	public void readKey() throws IOException {
+		String expectedOutput = "{\"key\":\"/test/testKey\",\"value\":\"testValue\"";
 
-    	String response = ETCDLibrary.ReadKey(directoryName, key);
-    	if(response.contains("{\"action\":\"set\",\"node\":{\"key\":\"/"+directoryName+"/"+key+"\""))
-    		return true;
-    	return false;
-    }
-    
-    private void createDirectory(String directoryName) throws IOException { 
-    	ETCDLibrary.CreateDirectory(directoryName);
-    }
-    
-    private void deleteDirectory(String directoryName) throws IOException { 
-    	ETCDLibrary.DeleteDirectory(directoryName);
-    }
-    
-    private void createKey(String directoryName, String key, String value) throws IOException { 
-    	ETCDLibrary.CreateKey(directoryName, key, value);
-    }
-    
-    private void deleteKey(String directoryName, String key) throws IOException { 
-    	ETCDLibrary.DeleteKey(directoryName, key);
-    }
-    
-    
-    
+		if (!keyExists(directoryName, key))
+			createKey(directoryName, key, value);
+
+		HttpResponse response = library.CreateKey("test", key, value);
+		String responseString = EntityUtils.toString(response.getEntity());
+    	
+    	assertTrue(responseString.contains(expectedOutput));
+    	
+		deleteKey(directoryName, key);
+
+	}
+
+	private boolean directoryExists(String directoryName) throws IOException {
+
+		HttpResponse response = library.ReadDirectory(directoryName);
+		String responseString = EntityUtils.toString(response.getEntity());
+    	
+    	if (responseString.contains("{\"action\":\"get\",\"node\":{\"key\":\"/" + directoryName + "\",\"dir\":true"))
+			return true;
+		return false;
+	}
+
+	private boolean keyExists(String directoryName, String key) throws IOException {
+
+		HttpResponse response = library.ReadKey(directoryName, key);
+		String responseString = EntityUtils.toString(response.getEntity());
+		if (responseString.contains("{\"action\":\"set\",\"node\":{\"key\":\"/" + directoryName + "/" + key + "\""))
+			return true;
+		return false;
+	}
+
+	private void createDirectory(String directoryName) throws IOException {
+		library.CreateDirectory(directoryName);
+	}
+
+	private void deleteDirectory(String directoryName) throws IOException {
+		library.DeleteDirectory(directoryName);
+	}
+
+	private void createKey(String directoryName, String key, String value) throws IOException {
+		library.CreateKey(directoryName, key, value);
+	}
+
+	private void deleteKey(String directoryName, String key) throws IOException {
+		library.DeleteKey(directoryName, key);
+	}
+
 }
