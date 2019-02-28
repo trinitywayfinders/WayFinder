e7e5d07b09a96932f2570bd15113227e1221048c
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-02-28 17:48:44 +0000

    WD-69: Corrected the test package structure in Env Metrics

--- Code/EnvironmentMetricsService/src/test/java/ie/tcd/wayfinder/envmetrics/weather/GeoHash/GeoHashFunctionsTest.java
+++ Code/EnvironmentMetricsService/src/test/java/ie/tcd/wayfinder/envmetrics/weather/GeoHash/GeoHashFunctionsTest.java
@@ -1,4 +1,4 @@
-package ie.tcd.wayfinder.envmetrics.weather;
+package ie.tcd.wayfinder.envmetrics.weather.GeoHash;
 
 import static org.junit.Assert.assertEquals;
 
@@ -7,69 +7,69 @@ import org.junit.Test;
 
 import ie.tcd.wayfinder.envmetrics.weather.GeoHash.GeoHashFunctions;
 
-public class GeoHashTest {
+public class GeoHashFunctionsTest {
 
 	private static GeoHashFunctions geohash;
 
 	@BeforeClass
 	public static void createGeoHashObject() {
-		GeoHashTest.geohash = new GeoHashFunctions();
+		GeoHashFunctionsTest.geohash = new GeoHashFunctions();
 	}
 
 	@Test
 	public void geohashTwoPositive() {
 		double lat = 2.79;
 		double lng = 1.34;
-		assertEquals(GeoHashTest.geohash.getGeoHash(lat, lng), "s02zy");
+		assertEquals(GeoHashFunctionsTest.geohash.getGeoHash(lat, lng), "s02zy");
 	}
 
 	@Test
 	public void geohashTwoNegative() {
 		double lat = -80.94;
 		double lng = -41.34;
-		assertEquals(GeoHashTest.geohash.getGeoHash(lat, lng), "51dee");
+		assertEquals(GeoHashFunctionsTest.geohash.getGeoHash(lat, lng), "51dee");
 	}
 
 	@Test
 	public void geohashNegativePositive() {
 		double lat = -81.34;
 		double lng = 49.03;
-		assertEquals(GeoHashTest.geohash.getGeoHash(lat, lng), "j1dc7");
+		assertEquals(GeoHashFunctionsTest.geohash.getGeoHash(lat, lng), "j1dc7");
 	}
 
 	@Test
 	public void geohashPositiveNegative() {
 		double lat = 1.21;
 		double lng = -3.84;
-		assertEquals(GeoHashTest.geohash.getGeoHash(lat, lng), "ebjqb");
+		assertEquals(GeoHashFunctionsTest.geohash.getGeoHash(lat, lng), "ebjqb");
 	}
 
 	@Test
 	public void geohashZeroPositive() {
 		double lat = 0;
 		double lng = 42.1;
-		assertEquals(GeoHashTest.geohash.getGeoHash(lat, lng), "sbjbn");
+		assertEquals(GeoHashFunctionsTest.geohash.getGeoHash(lat, lng), "sbjbn");
 	}
 
 	@Test
 	public void geohashZeroZero() {
 		double lat = 0;
 		double lng = 0;
-		assertEquals(GeoHashTest.geohash.getGeoHash(lat, lng), "s0000");
+		assertEquals(GeoHashFunctionsTest.geohash.getGeoHash(lat, lng), "s0000");
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void geohashOutOfBoundsLatitude() {
 		double lat = 91.29;
 		double lng = -149.73;
-		GeoHashTest.geohash.getGeoHash(lat, lng);
+		GeoHashFunctionsTest.geohash.getGeoHash(lat, lng);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void geohashOutOfBoundsLongitude() {
 		double lat = 41.3;
 		double lng = 191.31;
-		GeoHashTest.geohash.getGeoHash(lat, lng);
+		GeoHashFunctionsTest.geohash.getGeoHash(lat, lng);
 	}
 
 //	    @Test
