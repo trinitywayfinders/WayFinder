7e72eb38eabd95eddd1dd5553b8b2052dbb8b03b
maliksa <maliksa@tcd.ie>
2019-03-28 07:28:20 -0700

    userprefs controller

--- Code/UserPrefs/src/main/java/ie/tcd/wayfinder/userprefs/controller/UserPrefsController.java
+++ Code/UserPrefs/src/main/java/ie/tcd/wayfinder/userprefs/controller/UserPrefsController.java
@@ -1,15 +1,18 @@
-package ie.tcd.wayfinder.userprefs.controller;
+package com.wayfinder.userprefs;
+
+import java.util.HashSet;
 import java.util.Map;
 
-import ie.tcd.wayfinders.entities.UserPrefs;
-import ie.tcd.wayfinders.repositories.UserPrefsRepository;
+import com.wayfinder.userprefs.UserPrefs;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class UserPrefsController {
@@ -20,41 +23,231 @@ public class UserPrefsController {
 	@CrossOrigin(origins = "*")
 	@GetMapping(path="/api/getUserPrefs")
     public UserPrefs getUserPrefs(@RequestBody Map<String, Object> request) {
-    	//Get object from database, convert to POJO and return it
-		//UserPrefs userPrefsObj = new UserPrefs("saad",(short)1,(short)1,(short)1,(short)1,(short)1);
-		if (!request.containsKey("username")) {
-			return null;
+		if (!request.containsKey(UserPrefs.USERNAME_KEY)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Username not provided");
 		}
-		String username = (String)request.get("username");
-		// UserPrefs userPrefsObj = repo.findByUsername(username);
-		// return userPrefsObj;
-		return null;
+		String username = (String)request.get(UserPrefs.USERNAME_KEY);
+		UserPrefs userPrefsObj = repo.findByUsername(username);
+		if (userPrefsObj==null) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "User does not exist in DB.");
+		}
+		return userPrefsObj;
     }
 
 	@CrossOrigin(origins = "*")
 	@PostMapping(path="/api/setUserPrefs")
 	public UserPrefs setUserPrefs(@RequestBody Map<String, Object> request) {
-//    	//Get current user prefs from DB and conver to POJO
-//		UserPrefs userPrefsObj = new UserPrefs("saad",(short)1,(short)1,(short)1,(short)1,(short)1);
-//		//
-//		repo.save(userPrefsObj);
-//		if (request.containsKey("concernHealth")){
-//			userPrefsObj.setConcernHealth(Short.parseShort((String) request.get("concernHealth")));
-//		}
-//		if (request.containsKey("concernSpeed")){
-//			userPrefsObj.setConcernSpeed(Short.parseShort((String) request.get("concernSpeed")));
-//		}
-//		if (request.containsKey("concernCost")){
-//			userPrefsObj.setConcernCost(Short.parseShort((String) request.get("concernCost")));
-//		}
-//		if (request.containsKey("concernPollutionAvoidance")){
-//			userPrefsObj.setConcernPollutionAvoidance(Short.parseShort((String) request.get("concernPollutionAvoidance")));
-//		}
-//		if (request.containsKey("concernEmissionsReduction")){
-//			userPrefsObj.setConcernEmissionsReduction(Short.parseShort((String) request.get("concernEmissionsReduction")));
-//		}
-//		return userPrefsObj;
-		return null;
+		HashSet<Integer> vals = new HashSet<Integer>();
+		if (!request.containsKey(UserPrefs.USERNAME_KEY)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Username not provided");
+		}
+		String username = (String)request.get(UserPrefs.USERNAME_KEY);
+		
+		if (repo.findByUsername(username)!=null) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Username already exists in DB. Update preferences using updateUserPrefs endpoint");
+		}
+		
+		int concernHealth = 1;
+		if (request.containsKey(UserPrefs.HEALTH_KEY)){
+			concernHealth = (int) request.get(UserPrefs.HEALTH_KEY);
+			if (concernHealth<0 || concernHealth>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernHealth");
+			}
+		}
+		if(vals.contains(concernHealth)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernHealth has the same value as one other param");
+		}
+		vals.add(concernHealth);
+		
+		int concernSpeed = 2;
+		if (request.containsKey(UserPrefs.SPEED_KEY)){
+			concernSpeed = (int) request.get(UserPrefs.SPEED_KEY);
+			if (concernSpeed<0 || concernSpeed>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernSpeed");
+			}
+		}
+		if(vals.contains(concernSpeed)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernSpeed has the same value as one other param");
+		}
+		vals.add(concernSpeed);
+		
+		int concernCost = 3;
+		if (request.containsKey(UserPrefs.COST_KEY)){
+			concernCost = (int) request.get(UserPrefs.COST_KEY);
+			if (concernCost<0 || concernCost>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernCost");
+			}
+		}
+		if(vals.contains(concernCost)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernCost has the same value as one other param");
+		}
+		vals.add(concernCost);
+		
+		int concernPollutionAvoidance = 4;
+		if (request.containsKey(UserPrefs.POLLUTION_AVOIDANCE_KEY)){
+			concernPollutionAvoidance = (int) request.get(UserPrefs.POLLUTION_AVOIDANCE_KEY);
+			if (concernPollutionAvoidance<0 || concernPollutionAvoidance>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernPollutionAvoidance");
+			}
+		}
+		if(vals.contains(concernPollutionAvoidance)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernPollutionAvoidance has the same value as one other param");
+		}
+		vals.add(concernPollutionAvoidance);
+		
+		int concernEmissionsReduction = 5;
+		if (request.containsKey(UserPrefs.EMISSIONS_REDUCTION_KEY)){
+			concernEmissionsReduction = (int) request.get(UserPrefs.EMISSIONS_REDUCTION_KEY);
+			if (concernEmissionsReduction<0 || concernEmissionsReduction>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernEmissionsReduction");
+			}
+		}
+		if(vals.contains(concernEmissionsReduction)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernEmissionsReduction has the same value as one other param");
+		}
+		vals.add(concernEmissionsReduction);
+		
+		UserPrefs userPrefsObj = new UserPrefs(username,concernHealth,concernSpeed,concernCost,concernPollutionAvoidance,concernEmissionsReduction);
+		repo.save(userPrefsObj);
+		userPrefsObj = repo.findByUsername(username);
+		return userPrefsObj;
     }
-    
+	
+	@CrossOrigin(origins = "*")
+	@PostMapping(path="/api/updateUserPrefs")
+	public UserPrefs updateUserPrefs(@RequestBody Map<String, Object> request) {
+		HashSet<Integer> vals = new HashSet<Integer>();
+		
+		if (!request.containsKey(UserPrefs.USERNAME_KEY)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Username not in request");
+		}
+		String username = (String)request.get(UserPrefs.USERNAME_KEY);
+		
+		UserPrefs userPrefsObj = repo.findByUsername(username);
+		if (userPrefsObj==null) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "User does not exist in DB. Set user preferences first using the setUserPrefs endpoint");
+		}
+		
+		int concernHealth = userPrefsObj.getConcernHealth();
+		if (request.containsKey(UserPrefs.HEALTH_KEY)){
+			concernHealth = (int) request.get(UserPrefs.HEALTH_KEY);
+			if (concernHealth<0 || concernHealth>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernHealth");
+			}
+		}
+		if (vals.contains(concernHealth)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernHealth has same value as one other param");
+		}
+		userPrefsObj.setConcernHealth(concernHealth);
+		vals.add(concernHealth);
+		
+		int concernSpeed = userPrefsObj.getConcernSpeed();
+		if (request.containsKey(UserPrefs.SPEED_KEY)){
+			concernSpeed = (int) request.get(UserPrefs.SPEED_KEY);
+			if (concernSpeed<0 || concernSpeed>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernSpeed");
+			}
+		}
+		if (vals.contains(concernSpeed)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernSpeed has same value as one other param");
+		}
+		userPrefsObj.setConcernSpeed(concernSpeed);
+		vals.add(concernSpeed);
+		
+		int concernCost = userPrefsObj.getConcernCost();
+		if (request.containsKey(UserPrefs.COST_KEY)){
+			concernCost = (int) request.get(UserPrefs.COST_KEY);
+			if (concernCost<0 || concernCost>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernCost");
+			}
+		}
+		if (vals.contains(concernCost)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernCost has same value as one other param");
+		}
+		userPrefsObj.setConcernCost(concernCost);
+		vals.add(concernCost);
+		
+		int concernPollutionAvoidance = userPrefsObj.getConcernPollutionAvoidance();
+		if (request.containsKey(UserPrefs.POLLUTION_AVOIDANCE_KEY)){
+			concernPollutionAvoidance = (int) request.get(UserPrefs.POLLUTION_AVOIDANCE_KEY);
+			if (concernPollutionAvoidance<0 || concernPollutionAvoidance>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernPollutionAvoidance");
+			}
+		}
+		if (vals.contains(concernPollutionAvoidance)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernPollutionAvoidance has same value as one other param");
+		}
+		userPrefsObj.setConcernPollutionAvoidance(concernPollutionAvoidance);
+		vals.add(concernPollutionAvoidance);
+		
+		int concernEmissionsReduction = userPrefsObj.getConcernEmissionsReduction();
+		if (request.containsKey(UserPrefs.EMISSIONS_REDUCTION_KEY)){
+			concernEmissionsReduction = (int) request.get(UserPrefs.EMISSIONS_REDUCTION_KEY);
+			if (concernEmissionsReduction<0 || concernEmissionsReduction>5) {
+				throw new ResponseStatusException(
+				           HttpStatus.BAD_REQUEST, "Invalid value for concernEmissionsReduction");
+			}
+		}
+		if (vals.contains(concernEmissionsReduction)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "concernEmissionsReduction has same value as one other param");
+		}
+		userPrefsObj.setConcernEmissionsReduction(concernEmissionsReduction);
+		vals.add(concernEmissionsReduction);
+		
+		repo.save(userPrefsObj);
+		userPrefsObj = repo.findByUsername(username);
+		return userPrefsObj;
+    }
+
+	@CrossOrigin(origins = "*")
+	@PostMapping(path="/api/removeUserPrefs")
+	public boolean removeUserPrefs(@RequestBody Map<String, Object> request) {
+		boolean status = false;
+		if (!request.containsKey(UserPrefs.USERNAME_KEY)) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Username not in request");
+		}
+		String username = (String)request.get(UserPrefs.USERNAME_KEY);
+		
+		UserPrefs userPrefsObj = repo.findByUsername(username);
+		if (userPrefsObj==null) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "User does not exist in DB.");
+		}
+
+		try {
+			repo.delete(userPrefsObj);
+			status = true;
+		}
+		catch (Exception e) {
+		     status = false;
+		}
+		return status;
+    }
+	
 }
