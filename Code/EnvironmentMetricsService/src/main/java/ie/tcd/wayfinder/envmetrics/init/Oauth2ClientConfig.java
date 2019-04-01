d6a34eb5fa0a240c72847497aa71f0a920765b79
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-04-01 23:06:32 +0100

    WD-69: Re-enabled authentication

--- Code/EnvironmentMetricsService/src/main/java/ie/tcd/wayfinder/envmetrics/init/Oauth2ClientConfig.java
+++ Code/EnvironmentMetricsService/src/main/java/ie/tcd/wayfinder/envmetrics/init/Oauth2ClientConfig.java
@@ -7,11 +7,11 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
-//@Configuration
-//@EnableOAuth2Client
+@Configuration
+@EnableOAuth2Client
 public class Oauth2ClientConfig {
-//	@Bean
-//	public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
-//	    return new OAuth2RestTemplate(details, oauth2ClientContext);
-//	}
+	@Bean
+	public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+	    return new OAuth2RestTemplate(details, oauth2ClientContext);
+	}
 }
--- Code/EnvironmentMetricsService/src/main/java/ie/tcd/wayfinder/envmetrics/init/ResourceServerConfig.java
+++ Code/EnvironmentMetricsService/src/main/java/ie/tcd/wayfinder/envmetrics/init/ResourceServerConfig.java
@@ -5,18 +5,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-//@Configuration
-//@EnableResourceServer
-//public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-//	@Override
-//	public void configure(HttpSecurity http) throws Exception {
-//		http
-//			.requestMatchers().antMatchers("/**")
-//		.and()
-//			.authorizeRequests()
-//				.antMatchers("/**").authenticated()
-//		.and()
-//			.cors().disable();
-//	}
-//	
-//}
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.requestMatchers().antMatchers("/**")
+		.and()
+			.authorizeRequests()
+				.antMatchers("/**").authenticated()
+		.and()
+			.cors().disable();
+	}
+	
+}
